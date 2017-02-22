b88321c8e77ac0cf3236bf5977588aae8bad118d
Matt Barber <MattBarber@redweb.com>
2017-02-22 11:40:19 +0000

    Added watcher for file changes

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const ProcessCsv = require('./lib/ProcessCsv.js');
+const chokidar = require('chokidar');
+const watcher = chokidar.watch('~/public/*.csv', {
+  persistent: true
+});
 
+// When files are added to the ~/public/ folder of type CSV....
+watcher.on('add', function(path) {
+    //...We're processing....
+    console.log(`File ${path} added...processing`);
+    ProcessCsv.load(path)
+        .then(function(data) {
+            // Log stuff
+            console.log(ProcessCsv.top(3));
+            console.log(`${path} processed`);
+        }).catch(function (err) {
+            console.error(err);
+        });
 
-ProcessCsv.load('./1487702986724.csv')
-    .then(function(data) {
-        console.log(ProcessCsv.top(3));
-
-    }).catch(function (err) {
-        console.error(err);
-    });
+})
